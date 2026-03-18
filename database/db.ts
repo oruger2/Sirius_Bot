86e973ed28df61f1f2d9acfdf0874b6001e04027
オルガー <135433478+oruger-0730@users.noreply.github.com>
2026-03-18 07:12:35 +0000

    add

---
+++ database/db.ts
@@ -0,0 +1,17 @@
+import mysql from "mysql2/promise";
+import type { Pool } from "mysql2/promise"; 
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool: Pool = mysql.createPool({
+  host: process.env.DB_HOST as string,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PASSWORD as string,
+  database: process.env.DB_NAME as string,
+  waitForConnections: true,
+  connectionLimit: 5,
+});
+
+export default pool;
