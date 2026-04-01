7e929a60740af2e852317014f06df18841b918a6
オルガー <135433478+oruger-0730@users.noreply.github.com>
2026-04-01 22:32:38 +0000

    fix user.ts

--- src/commands/user.ts
+++ src/commands/user.ts
@@ -1,10 +1,10 @@
 import {
-  EmbedBuilder,
-  SlashCommandBuilder,
-  type ChatInputCommandInteraction,
-  type Guild,
-  type GuildMember,
-  type User
+  type ChatInputCommandInteraction,  
+  EmbedBuilder,  
+  type Guild,  
+  type GuildMember,  
+  SlashCommandBuilder,  
+  type User,
 } from "discord.js";
 
 import { SUCCESS_ICON_URL } from "@/utils/embedIcons";
