9de90ad612eae554dd1aaecc538a6e09d2a6445e
オルガー <135433478+oruger-0730@users.noreply.github.com>
2026-04-07 15:44:41 +0900

    Update src/events/settingInteraction.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/events/settingInteraction.ts
+++ src/events/settingInteraction.ts
@@ -9,16 +9,16 @@ import {
 } from "@/commands/setting";
 
 export default {
-  name: "interactionCreate",
-  async execute(interaction: Interaction) {
-    if (interaction.isButton()) {
-      try {
-        await handleSettingButtonInteraction(interaction);
-      } catch (error) {
-        console.error("setting button handler error:", error);
-      }
-      return;
-    }
+	name: "interactionCreate",
+	async execute(interaction: Interaction) {
+		if (interaction.isButton()) {
+			try {
+				await handleSettingButtonInteraction(interaction);
+			} catch (error) {
+				console.error("setting button handler error:", error);
+			}
+			return;
+		}
 
     if (!interaction.isModalSubmit()) return;
 
