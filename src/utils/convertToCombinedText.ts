9ba26181ced27c5d381db7cbde9e0c22b600c9f8
オルガー <135433478+oruger-0730@users.noreply.github.com>
2026-04-20 14:27:06 +0900

    Update src/utils/convertToCombinedText.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/utils/convertToCombinedText.ts
+++ src/utils/convertToCombinedText.ts
@@ -1,4 +1,4 @@
-import type { Message } from 'discord.js';
+import type { Message } from "discord.js";
 
 export default function convertToCombinedText (message: Message) {
 	const embedText = message.embeds
