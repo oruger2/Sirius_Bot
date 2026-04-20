9553a41aef3e27e5f4d0a8c5c49d89541ee15733
オルガー <135433478+oruger-0730@users.noreply.github.com>
2026-04-20 14:39:51 +0900

    Update src/utils/scheduleReminder.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/utils/scheduleReminder.ts
+++ src/utils/scheduleReminder.ts
@@ -1,4 +1,4 @@
-import type { TextChannel } from 'discord.js';
+import type { TextChannel } from "discord.js";
 
 export default function scheduleReminder(channel: TextChannel, content: string, delay: number) {
   if (delay < 0) throw new RangeError('Delay must be a non-negative number');
