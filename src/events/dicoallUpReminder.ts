5c1b378587c737740aa1db94276cb463587657c3
オルガー <135433478+oruger-0730@users.noreply.github.com>
2026-04-20 14:26:18 +0900

    Update src/events/dicoallUpReminder.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/events/dicoallUpReminder.ts
+++ src/events/dicoallUpReminder.ts
@@ -1,6 +1,6 @@
-import type { Messsage } from 'discord.js';
-import scheduleReminder from '../utils/scheduleReminder';
-import convertToCombinedText from '../utils/convertToCombinedText';
+import type { Messsage } from "discord.js";
+import convertToCombinedText from "../utils/convertToCombinedText";
+import scheduleReminder from "../utils/scheduleReminder";
 
 const REMINDER_INTERVAL = 1 * 60 * 60 * 1000;
 const DICOALL_BOT_ID = '903541413298450462';
