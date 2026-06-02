2dc6105295c86733c336b7eb27b7009fa201bd6e
オルガー <135433478+oruger-0730@users.noreply.github.com>
2026-06-03 08:14:33 +0900

    Update src/commands/survival.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/commands/survival.ts
+++ src/commands/survival.ts
@@ -23,416 +23,416 @@ interface GameState {
 	mustEat: boolean;
 }
 const foods: Food[] = [
-{
-    name: "おにぎり",
-    danger: 0,
-    deathReason: "おにぎりが喉に詰まった",
-},
-{
-    name: "ハンバーガー",
-    danger: 1,
-    deathReason: "ハンバーガーを食べすぎた",
-},
-{
-    name: "カレー",
-    danger: 2,
-    deathReason: "カレーを飲み込むのに失敗した",
-},
-{
-    name: "ラーメン",
-    danger: 3,
-    deathReason: "ラーメンのスープでむせた",
-},
-{
-    name: "うどん",
-    danger: 4,
-    deathReason: "うどんを勢いよくすすりすぎた",
-},
-{
-    name: "寿司",
-    danger: 5,
-    deathReason: "寿司を丸飲みした",
-},
-{
-    name: "ピザ",
-    danger: 6,
-    deathReason: "ピザを食べすぎた",
-},
-{
-    name: "唐揚げ",
-    danger: 7,
-    deathReason: "唐揚げが喉に詰まった",
-},
-{
-    name: "ケーキ",
-    danger: 8,
-    deathReason: "ケーキを一気食いした",
-},
-{
-    name: "アイス",
-    danger: 9,
-    deathReason: "頭がキーンとなりすぎた",
-},
+	{
+		name: "おにぎり",
+		danger: 0,
+		deathReason: "おにぎりが喉に詰まった",
+	},
+	{
+		name: "ハンバーガー",
+		danger: 1,
+		deathReason: "ハンバーガーを食べすぎた",
+	},
+	{
+		name: "カレー",
+		danger: 2,
+		deathReason: "カレーを飲み込むのに失敗した",
+	},
+	{
+		name: "ラーメン",
+		danger: 3,
+		deathReason: "ラーメンのスープでむせた",
+	},
+	{
+		name: "うどん",
+		danger: 4,
+		deathReason: "うどんを勢いよくすすりすぎた",
+	},
+	{
+		name: "寿司",
+		danger: 5,
+		deathReason: "寿司を丸飲みした",
+	},
+	{
+		name: "ピザ",
+		danger: 6,
+		deathReason: "ピザを食べすぎた",
+	},
+	{
+		name: "唐揚げ",
+		danger: 7,
+		deathReason: "唐揚げが喉に詰まった",
+	},
+	{
+		name: "ケーキ",
+		danger: 8,
+		deathReason: "ケーキを一気食いした",
+	},
+	{
+		name: "アイス",
+		danger: 9,
+		deathReason: "頭がキーンとなりすぎた",
+	},
 
-{
-    name: "消しゴム",
-    danger: 12,
-    deathReason: "消しゴムが喉に詰まった",
-},
-{
-    name: "ティッシュ",
-    danger: 14,
-    deathReason: "ティッシュが気管を塞いだ",
-},
-{
-    name: "紙",
-    danger: 15,
-    deathReason: "紙を飲み込みすぎた",
-},
-{
-    name: "段ボール",
-    danger: 18,
-    deathReason: "段ボールが胃で膨張した",
-},
-{
-    name: "綿あめみたいな綿",
-    danger: 19,
-    deathReason: "綿が喉に絡まった",
-},
-{
-    name: "クレヨン",
-    danger: 11,
-    deathReason: "クレヨンを食べ続けた",
-},
-{
-    name: "色鉛筆",
-    danger: 22,
-    deathReason: "色鉛筆の芯が刺さった",
-},
-{
-    name: "鉛筆",
-    danger: 25,
-    deathReason: "鉛筆を噛み砕こうとした",
-},
-{
-    name: "チョーク",
-    danger: 20,
-    deathReason: "チョークの粉でむせた",
-},
-{
-    name: "スライム",
-    danger: 14,
-    deathReason: "スライムが気管に入った",
-},
+	{
+		name: "消しゴム",
+		danger: 12,
+		deathReason: "消しゴムが喉に詰まった",
+	},
+	{
+		name: "ティッシュ",
+		danger: 14,
+		deathReason: "ティッシュが気管を塞いだ",
+	},
+	{
+		name: "紙",
+		danger: 15,
+		deathReason: "紙を飲み込みすぎた",
+	},
+	{
+		name: "段ボール",
+		danger: 18,
+		deathReason: "段ボールが胃で膨張した",
+	},
+	{
+		name: "綿あめみたいな綿",
+		danger: 19,
+		deathReason: "綿が喉に絡まった",
+	},
+	{
+		name: "クレヨン",
+		danger: 11,
+		deathReason: "クレヨンを食べ続けた",
+	},
+	{
+		name: "色鉛筆",
+		danger: 22,
+		deathReason: "色鉛筆の芯が刺さった",
+	},
+	{
+		name: "鉛筆",
+		danger: 25,
+		deathReason: "鉛筆を噛み砕こうとした",
+	},
+	{
+		name: "チョーク",
+		danger: 20,
+		deathReason: "チョークの粉でむせた",
+	},
+	{
+		name: "スライム",
+		danger: 14,
+		deathReason: "スライムが気管に入った",
+	},
 
-{
-    name: "のり",
-    danger: 16,
-    deathReason: "のりで口が塞がった",
-},
-{
-    name: "石鹸",
-    danger: 24,
-    deathReason: "石鹸の泡で呼吸できなくなった",
-},
-{
-    name: "ロウソク",
-    danger: 26,
-    deathReason: "ロウが喉で固まった",
-},
-{
-    name: "木の枝",
-    danger: 28,
-    deathReason: "木の破片が刺さった",
-},
-{
-    name: "ビー玉",
-    danger: 17,
-    deathReason: "ビー玉が喉に詰まった",
-},
-{
-    name: "ボタン",
-    danger: 24,
-    deathReason: "ボタンを飲み込んだ",
-},
-{
-    name: "輪ゴム",
-    danger: 15,
-    deathReason: "輪ゴムが胃で絡まった",
-},
-{
-    name: "粘土",
-    danger: 32,
-    deathReason: "粘土が胃で固まった",
-},
-{
-    name: "乾電池",
-    danger: 65,
-    deathReason: "乾電池の中身が漏れた",
-},
-{
-    name: "蛍光ペン",
-    danger: 42,
-    deathReason: "インクを大量摂取した",
-},
-{
-    name: "発泡スチロール",
-    danger: 33,
-    deathReason: "発泡スチロールが喉に詰まった",
-},
-{
-    name: "プラスチックスプーン",
-    danger: 30,
-    deathReason: "スプーンの破片が刺さった",
-},
-{
-    name: "プラスチックフォーク",
-    danger: 35,
-    deathReason: "フォークを噛み砕こうとした",
-},
-{
-    name: "ストロー",
-    danger: 18,
-    deathReason: "ストローが気管に入った",
-},
-{
-    name: "割り箸",
-    danger: 21,
-    deathReason: "割り箸の破片が刺さった",
-},
-{
-    name: "爪楊枝",
-    danger: 30,
-    deathReason: "爪楊枝が喉に刺さった",
-},
-{
-    name: "ヘアゴム",
-    danger: 27,
-    deathReason: "ヘアゴムが胃で絡まった",
-},
-{
-    name: "セロハンテープ",
-    danger: 27,
-    deathReason: "口が塞がった",
-},
-{
-    name: "ガムテープ",
-    danger: 35,
-    deathReason: "ガムテープが消化できなかった",
-},
-{
-    name: "接着剤",
-    danger: 47,
-    deathReason: "接着剤で内臓が固まった",
-},
-{
-    name: "洗濯のり",
-    danger: 48,
-    deathReason: "胃の中がベタベタになった",
-},
-{
-    name: "石鹸",
-    danger: 34,
-    deathReason: "泡で呼吸できなくなった",
-},
-{
-    name: "シャンプー",
-    danger: 31,
-    deathReason: "シャンプーを飲みすぎた",
-},
-{
-    name: "ボディソープ",
-    danger: 23,
-    deathReason: "胃が泡だらけになった",
-},
-{
-    name: "歯磨き粉",
-    danger: 10,
-    deathReason: "歯磨き粉を大量摂取した",
-},
-{
-    name: "口紅",
-    danger: 14,
-    deathReason: "口紅を一本丸ごと食べた",
-},
-{
-    name: "シャーペンの芯",
-    danger: 12,
-    deathReason: "シャーペンの芯が刺さった",
-},
-{
-    name: "シャーペン",
-    danger: 11,
-    deathReason: "シャーペンを食べ続けた",
-},
-{
-    name: "カニ",
-    danger: 26,
-    deathReason: "カニを食べすぎた",
-},
-{
-    name: "ワイン",
-    danger: 17,
-    deathReason: "ワインを飲み干した",
-},
-{
-    name: "フライパン",
-    danger: 44,
-    deathReason: "フライパンが喉に詰まった",
-},
-{
-    name: "人形",
-    danger: 37,
-    deathReason: "人形を大量摂取した",
-},
-{
-    name: "教科書",
-    danger: 26,
-    deathReason: "紙が喉に詰まった",
-},
-{
-    name: "消しゴムのカス",
-    danger: 11,
-    deathReason: "消しゴムのカスが気管に入った",
-},
-{
-    name: "カッター",
-    danger: 40,
-    deathReason: "カッターが喉を突き破った",
-},
-{
-    name: "おはじき",
-    danger: 32,
-    deathReason: "おはじきを飲み込んだ",
-},
-{
-    name: "スーパーボール",
-    danger: 38,
-    deathReason: "スーパーボールが喉に詰まった",
-},
-{
-    name: "粘土",
-    danger: 48,
-    deathReason: "粘土が胃で固まった",
-},
-{
-    name: "紙粘土",
-    danger: 38,
-    deathReason: "紙粘土が膨張した",
-},
-{
-    name: "木炭",
-    danger: 42,
-    deathReason: "木炭を大量に食べた",
-},
-{
-    name: "木片",
-    danger: 22,
-    deathReason: "木片が刺さった",
-},
-{
-    name: "落ち葉",
-    danger: 16,
-    deathReason: "落ち葉を食べすぎた",
-},
-{
-    name: "芝生",
-    danger: 13,
-    deathReason: "芝生が消化できなかった",
-},
-{
-    name: "観葉植物",
-    danger: 15,
-    deathReason: "観葉植物に反撃された",
-},
-{
-    name: "花びら",
-    danger: 10,
-    deathReason: "花びらを喉に詰まらせた",
-},
-{
-    name: "松ぼっくり",
-    danger:35,
-    deathReason: "松ぼっくりを丸飲みした",
-},
-{
-    name: "どんぐり",
-    danger: 23,
-    deathReason: "どんぐりが喉に詰まった",
-},
-{
-    name: "小石",
-    danger: 25,
-    deathReason: "小石で歯が砕けた",
-},
-{
-    name: "砂",
-    danger: 31,
-    deathReason: "砂を大量に吸い込んだ",
-},
-{
-    name: "泥団子",
-    danger: 29,
-    deathReason: "泥団子を食べた",
-},
-{
-    name: "レンガの欠片",
-    danger: 64,
-    deathReason: "レンガの破片で口の中が傷だらけになった",
-},
+	{
+		name: "のり",
+		danger: 16,
+		deathReason: "のりで口が塞がった",
+	},
+	{
+		name: "石鹸",
+		danger: 24,
+		deathReason: "石鹸の泡で呼吸できなくなった",
+	},
+	{
+		name: "ロウソク",
+		danger: 26,
+		deathReason: "ロウが喉で固まった",
+	},
+	{
+		name: "木の枝",
+		danger: 28,
+		deathReason: "木の破片が刺さった",
+	},
+	{
+		name: "ビー玉",
+		danger: 17,
+		deathReason: "ビー玉が喉に詰まった",
+	},
+	{
+		name: "ボタン",
+		danger: 24,
+		deathReason: "ボタンを飲み込んだ",
+	},
+	{
+		name: "輪ゴム",
+		danger: 15,
+		deathReason: "輪ゴムが胃で絡まった",
+	},
+	{
+		name: "粘土",
+		danger: 32,
+		deathReason: "粘土が胃で固まった",
+	},
+	{
+		name: "乾電池",
+		danger: 65,
+		deathReason: "乾電池の中身が漏れた",
+	},
+	{
+		name: "蛍光ペン",
+		danger: 42,
+		deathReason: "インクを大量摂取した",
+	},
+	{
+		name: "発泡スチロール",
+		danger: 33,
+		deathReason: "発泡スチロールが喉に詰まった",
+	},
+	{
+		name: "プラスチックスプーン",
+		danger: 30,
+		deathReason: "スプーンの破片が刺さった",
+	},
+	{
+		name: "プラスチックフォーク",
+		danger: 35,
+		deathReason: "フォークを噛み砕こうとした",
+	},
+	{
+		name: "ストロー",
+		danger: 18,
+		deathReason: "ストローが気管に入った",
+	},
+	{
+		name: "割り箸",
+		danger: 21,
+		deathReason: "割り箸の破片が刺さった",
+	},
+	{
+		name: "爪楊枝",
+		danger: 30,
+		deathReason: "爪楊枝が喉に刺さった",
+	},
+	{
+		name: "ヘアゴム",
+		danger: 27,
+		deathReason: "ヘアゴムが胃で絡まった",
+	},
+	{
+		name: "セロハンテープ",
+		danger: 27,
+		deathReason: "口が塞がった",
+	},
+	{
+		name: "ガムテープ",
+		danger: 35,
+		deathReason: "ガムテープが消化できなかった",
+	},
+	{
+		name: "接着剤",
+		danger: 47,
+		deathReason: "接着剤で内臓が固まった",
+	},
+	{
+		name: "洗濯のり",
+		danger: 48,
+		deathReason: "胃の中がベタベタになった",
+	},
+	{
+		name: "石鹸",
+		danger: 34,
+		deathReason: "泡で呼吸できなくなった",
+	},
+	{
+		name: "シャンプー",
+		danger: 31,
+		deathReason: "シャンプーを飲みすぎた",
+	},
+	{
+		name: "ボディソープ",
+		danger: 23,
+		deathReason: "胃が泡だらけになった",
+	},
+	{
+		name: "歯磨き粉",
+		danger: 10,
+		deathReason: "歯磨き粉を大量摂取した",
+	},
+	{
+		name: "口紅",
+		danger: 14,
+		deathReason: "口紅を一本丸ごと食べた",
+	},
+	{
+		name: "シャーペンの芯",
+		danger: 12,
+		deathReason: "シャーペンの芯が刺さった",
+	},
+	{
+		name: "シャーペン",
+		danger: 11,
+		deathReason: "シャーペンを食べ続けた",
+	},
+	{
+		name: "カニ",
+		danger: 26,
+		deathReason: "カニを食べすぎた",
+	},
+	{
+		name: "ワイン",
+		danger: 17,
+		deathReason: "ワインを飲み干した",
+	},
+	{
+		name: "フライパン",
+		danger: 44,
+		deathReason: "フライパンが喉に詰まった",
+	},
+	{
+		name: "人形",
+		danger: 37,
+		deathReason: "人形を大量摂取した",
+	},
+	{
+		name: "教科書",
+		danger: 26,
+		deathReason: "紙が喉に詰まった",
+	},
+	{
+		name: "消しゴムのカス",
+		danger: 11,
+		deathReason: "消しゴムのカスが気管に入った",
+	},
+	{
+		name: "カッター",
+		danger: 40,
+		deathReason: "カッターが喉を突き破った",
+	},
+	{
+		name: "おはじき",
+		danger: 32,
+		deathReason: "おはじきを飲み込んだ",
+	},
+	{
+		name: "スーパーボール",
+		danger: 38,
+		deathReason: "スーパーボールが喉に詰まった",
+	},
+	{
+		name: "粘土",
+		danger: 48,
+		deathReason: "粘土が胃で固まった",
+	},
+	{
+		name: "紙粘土",
+		danger: 38,
+		deathReason: "紙粘土が膨張した",
+	},
+	{
+		name: "木炭",
+		danger: 42,
+		deathReason: "木炭を大量に食べた",
+	},
+	{
+		name: "木片",
+		danger: 22,
+		deathReason: "木片が刺さった",
+	},
+	{
+		name: "落ち葉",
+		danger: 16,
+		deathReason: "落ち葉を食べすぎた",
+	},
+	{
+		name: "芝生",
+		danger: 13,
+		deathReason: "芝生が消化できなかった",
+	},
+	{
+		name: "観葉植物",
+		danger: 15,
+		deathReason: "観葉植物に反撃された",
+	},
+	{
+		name: "花びら",
+		danger: 10,
+		deathReason: "花びらを喉に詰まらせた",
+	},
+	{
+		name: "松ぼっくり",
+		danger: 35,
+		deathReason: "松ぼっくりを丸飲みした",
+	},
+	{
+		name: "どんぐり",
+		danger: 23,
+		deathReason: "どんぐりが喉に詰まった",
+	},
+	{
+		name: "小石",
+		danger: 25,
+		deathReason: "小石で歯が砕けた",
+	},
+	{
+		name: "砂",
+		danger: 31,
+		deathReason: "砂を大量に吸い込んだ",
+	},
+	{
+		name: "泥団子",
+		danger: 29,
+		deathReason: "泥団子を食べた",
+	},
+	{
+		name: "レンガの欠片",
+		danger: 64,
+		deathReason: "レンガの破片で口の中が傷だらけになった",
+	},
 
-// 71～80
+	// 71～80
 
-{
-    name: "自転車",
-    danger: 71,
-    deathReason: "自転車を食べようとして顎が壊れた",
-},
-{
-    name: "原付",
-    danger: 72,
-    deathReason: "原付を食べようとして潰された",
-},
-{
-    name: "バイク",
-    danger: 73,
-    deathReason: "バイクを飲み込もうとした",
-},
-{
-    name: "軽自動車",
-    danger: 74,
-    deathReason: "軽自動車を食べるのは無理だった",
-},
-{
-    name: "普通車",
-    danger: 75,
-    deathReason: "車を噛んだ瞬間に敗北した",
-},
-{
-    name: "トラック",
-    danger: 76,
-    deathReason: "トラックに返り討ちにされた",
-},
-{
-    name: "電車",
-    danger: 77,
-    deathReason: "電車を食べようとした結果だった",
-},
-{
-    name: "冷蔵庫",
-    danger: 78,
-    deathReason: "冷蔵庫が大きすぎた",
-},
-{
-    name: "洗濯機",
-    danger: 79,
-    deathReason: "洗濯機を食べる前に力尽きた",
-},
-{
-    name: "一戸建て住宅",
-    danger: 80,
-    deathReason: "家は食べ物ではなかった",
-}
+	{
+		name: "自転車",
+		danger: 71,
+		deathReason: "自転車を食べようとして顎が壊れた",
+	},
+	{
+		name: "原付",
+		danger: 72,
+		deathReason: "原付を食べようとして潰された",
+	},
+	{
+		name: "バイク",
+		danger: 73,
+		deathReason: "バイクを飲み込もうとした",
+	},
+	{
+		name: "軽自動車",
+		danger: 74,
+		deathReason: "軽自動車を食べるのは無理だった",
+	},
+	{
+		name: "普通車",
+		danger: 75,
+		deathReason: "車を噛んだ瞬間に敗北した",
+	},
+	{
+		name: "トラック",
+		danger: 76,
+		deathReason: "トラックに返り討ちにされた",
+	},
+	{
+		name: "電車",
+		danger: 77,
+		deathReason: "電車を食べようとした結果だった",
+	},
+	{
+		name: "冷蔵庫",
+		danger: 78,
+		deathReason: "冷蔵庫が大きすぎた",
+	},
+	{
+		name: "洗濯機",
+		danger: 79,
+		deathReason: "洗濯機を食べる前に力尽きた",
+	},
+	{
+		name: "一戸建て住宅",
+		danger: 80,
+		deathReason: "家は食べ物ではなかった",
+	},
 ];
 
 function randomFood() {
