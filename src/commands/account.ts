263246aa8dd357e85287c8fe9dfbc6a846b702ae
oruger2 <support.lasting@gmail.com>
2026-06-19 07:33:34 +0000

    アカウント登録コマンドを追加し、経済アカウントの情報表示機能を実装しました

---
+++ src/commands/account.ts
@@ -0,0 +1,43 @@
+import {
+    type ChatInputCommandInteraction,
+	EmbedBuilder,
+	type GuildMember,
+	SlashCommandBuilder,
+    ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+}   from "discord.js";
+
+const command = {
+    data: new SlashCommandBuilder()
+    .setName("account")
+    .setDescription("経済アカウントについての情報を表示します")
+
+    .addSubcommand((sub) =>
+			sub
+				.setName("register")
+				.setDescription("経済アカウントを登録します"),
+		)
+}
+
+async function execute(interaction: ChatInputCommandInteraction) {
+    const subcommand = interaction.options.getSubcommand();
+
+    if (subcommand === "register") {
+        // アカウント登録処理をここに追加
+        const successEmbed = new EmbedBuilder()
+            .setColor("Blue")
+            .setTitle("アカウント登録")
+            .setDescription("経済アカウントを登録するには以下のボタンをクリックしてください。")
+
+        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+                    new ButtonBuilder()
+                        .setLabel("アカウント登録ページへ")
+                        .setStyle(ButtonStyle.Link)
+                        .setURL("https://siriusbot.f5.si/register") // アカウント登録ページのURLに置き換えてください,
+                );
+        await interaction.reply({ embeds: [successEmbed], ephemeral: true });
+    }
+}
+
+export { command, execute };
